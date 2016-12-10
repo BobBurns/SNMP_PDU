2665a133f4cc5074ab7c91f73ef5ba8c6c16ac9a
Bob Burns <bob.burns22@icloud.com>
2016-12-10 08:38:32 -0800

    add hello3 on branch opsmacA

---
+++ hello3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	printf("hello world!\n");
+	printf("goodbye world!\n");
+
+	int i;
+	for (i=0; i< 10; i++) {
+		printf("this is line %d \n", i);
+	}
+
+	printf("okay, now we have 3, from ops_mac\n");
+
+
+	return 0;
+}
+
